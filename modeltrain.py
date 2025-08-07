3290d38d342d65b5be9b84a80cce78b780d02016
gitshib123 <139163714+gitshib123@users.noreply.github.com>
2025-08-08 01:47:55 +0530

    Add files via upload

---
+++ modeltrain.py
@@ -0,0 +1,65 @@
+import tensorflow as tf # type: ignore
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout # type: ignore
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+
+# Define image dimensions and other parameters
+img_width, img_height = 48, 48
+batch_size = 32
+epochs = 20
+
+# Define directories for training and testing data
+train_data_dir = 'C:\Users\shibs\Downloads\Emotion Detection\ttrain'
+test_data_dir = 'C:\Users\shibs\Downloads\Emotion Detection\ttest'
+
+# Create data generators for training and testing data
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+test_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+# Build the CNN model
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(img_width, img_height, 3)),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(train_generator.num_classes, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+history = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // batch_size,
+    epochs=epochs,
+    validation_data=test_generator,
+    validation_steps=test_generator.samples // batch_size
+)
+
+# Save the trained model
+model.save('emotion_detection_model.h5')
